5b05580af7bb8eef35ebbb01b564d9b2de5fc116
Tih777 <mhmmdf4th@gmail.com>
2023-07-11 16:28:59 +0700

    perbaikan Landing Page

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('dashboard');
-});
-// Route::get('/home',[HomeController::class,'index']);
+// Route::get('/home', function () {
+//     return view('dashboard');
+// });
+Route::get('/home',[HomeController::class,'index']);
